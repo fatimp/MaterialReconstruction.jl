94504e6c9f8ef7bc171a81c73693730f47069799
Vasily Postnicov <shamaz.mazum@gmail.com>
2021-07-28 15:27:15 +0300

    Simplify čapek_cost

--- src/cost.jl
+++ src/cost.jl
@@ -1,20 +1,19 @@
 metric(x :: AbstractArray, y :: AbstractArray) =
     mapreduce((x, y) -> (x - y)^2, +, x, y)
 
-function sumcosts(tracker1 :: CorrelationTracker,
-                  tracker2 :: CorrelationTracker,
-                  costfn   :: Function,
-                           :: Nothing)
+function sumcosts(tracker1 :: CorrelationTracker{T, N},
+                  tracker2 :: CorrelationTracker{T, N},
+                  costfn   :: Function) where {T, N}
     @assert tracked_data(tracker1) == tracked_data(tracker2)
     return mapreduce(data -> costfn(tracker1 |> data,
                                     tracker2 |> data),
                      +, tracked_data(tracker1))
 end
 
-function sumcosts(tracker1 :: CorrelationTracker,
-                  tracker2 :: CorrelationTracker,
+function sumcosts(tracker1 :: CorrelationTracker{T, N},
+                  tracker2 :: CorrelationTracker{T, N},
                   costfn   :: Function,
-                  weights  :: Dict{AbstractTracker, Float64})
+                  weights  :: Dict{AbstractTracker{T}, Float64}) where {T, N}
     @assert tracked_data(tracker1) == tracked_data(tracker2)
     return mapreduce(data -> costfn(tracker1 |> data,
                                     tracker2 |> data) / weights[data],
@@ -36,11 +35,13 @@ along all directions before calculation.
 """
 function euclid_mean end
 
-euclid_mean(data1 :: CorrelationData, data2 :: CorrelationData) =
+euclid_mean(data1 :: CorrelationData{T},
+            data2 :: CorrelationData{T}) where T =
     metric(data1 |> mean, data2 |> mean)
 
-euclid_mean(data1 :: CorrelationTracker, data2 :: CorrelationTracker) =
-    sumcosts(data1, data2, euclid_mean, nothing)
+euclid_mean(data1 :: CorrelationTracker{T, N},
+            data2 :: CorrelationTracker{T, N}) where {T, N} =
+                sumcosts(data1, data2, euclid_mean)
 
 """
     euclid_directional(data1 :: CorrelationData, data2 :: CorrelationData)
@@ -53,15 +54,15 @@ along different directions are not averaged and treated separately.
 """
 function euclid_directional end
 
-function euclid_directional(data1 :: CorrelationData,
-                            data2 :: CorrelationData)
+function euclid_directional(data1 :: CorrelationData{T},
+                            data2 :: CorrelationData{T}) where T
     @assert directions(data1) == directions(data2)
     sum(metric(data1[dir], data2[dir]) for dir in directions(data1))
 end
 
-function euclid_directional(data1 :: CorrelationTracker,
-                            data2 :: CorrelationTracker)
-    sumcosts(data1, data2, euclid_directional, nothing)
+function euclid_directional(data1 :: CorrelationTracker{T, N},
+                            data2 :: CorrelationTracker{T, N}) where {T, N}
+    sumcosts(data1, data2, euclid_directional)
 end
 
 """
@@ -75,8 +76,8 @@ along all directions before calculation.
 Each correlation function has its own weight as described in
 Kirill M. Gerke and Marina V. Karsanina 2015 EPL 111 56002
 """
-function euclid_mean_weighted(data1 :: CorrelationTracker,
-                              data2 :: CorrelationTracker)
+function euclid_mean_weighted(data1 :: CorrelationTracker{T, N},
+                              data2 :: CorrelationTracker{T, N}) where {T, N}
     @assert tracked_data(data1) == tracked_data(data2)
     # At first, calculate weights for all correlation functions
     weights = Dict(data => euclid_mean(data1 |> data,
@@ -97,53 +98,14 @@ along different directions are not averaged and treated separately.
 Each correlation function has its own weight as described in
 Kirill M. Gerke and Marina V. Karsanina 2015 EPL 111 56002
 """
-function euclid_directional_weighted(data1 :: CorrelationTracker,
-                                     data2 :: CorrelationTracker)
+function euclid_directional_weighted(data1 :: CorrelationTracker{T, N},
+                                     data2 :: CorrelationTracker{T, N}) where {T, N}
     @assert tracked_data(data1) == tracked_data(data2)
     # At first, calculate weights for all correlation functions
     weights = Dict(data => euclid_directional(data1 |> data,
                                               data2 |> data)
                    for data in tracked_data(data1))
     f(:: Any, :: Any) = sumcosts(data1, data2, euclid_directional, weights)
-    return f
-end
-
-"""
-    čapek_cost(data1 :: CorrelationTracker, data2 :: CorrelationTracker, η = 0.6)
-
-Returns a function which calculates the cost as based on S₂ and L₂ for
-solid and void phases where contribution of L₂ for void phase
-increases with time.
-"""
-function čapek_cost(data1 :: CorrelationTracker,
-                    data2 :: CorrelationTracker,
-                    η     :: Float64 = 0.6)
-    # Initial S2
-    s2cost_init  = euclid_directional(Directional.s2(data1, 0),
-                                      Directional.s2(data2, 0))
-    # Initial L2 for solid phase
-    l2scost_init = euclid_directional(Directional.l2(data1, 1),
-                                      Directional.l2(data2, 1))
-
-    # Rescale η, so η = 1 means that a contribution of L2^{void} to
-    # the cost is initially halved. The reasonable values for η is
-    # then in the range [0, 1].
-    η = η*(s2cost_init + l2scost_init)
-
-    function f(data1 :: CorrelationTracker, data2 :: CorrelationTracker)
-        # S2
-        s2cost  = euclid_directional(Directional.s2(data1, 0),
-                                     Directional.s2(data2, 0))
-        # L2 for solid phase
-        l2scost = euclid_directional(Directional.l2(data1, 1),
-                                     Directional.l2(data2, 1))
-        # L2 for void phase
-        l2vcost = euclid_directional(Directional.l2(data1, 0),
-                                     Directional.l2(data2, 0))
-
-        return s2cost + l2scost + l2vcost*η/(η + s2cost + l2scost)
-    end
-
     return f
 end
 
@@ -157,9 +119,9 @@ Float64`. Values of `dict` must be in range [0, 1]. They control
 initial contribution of the corresponding correlation functions. The
 smaller these values are the smaller is initial contribution.
 """
-function generalized_čapek_cost(data1 :: CorrelationTracker,
-                                data2 :: CorrelationTracker,
-                                dict  :: Dict{<:AbstractTracker, Float64})
+function generalized_čapek_cost(data1 :: CorrelationTracker{T, N},
+                                data2 :: CorrelationTracker{T, N},
+                                dict  :: Dict{<:AbstractTracker{T}, Float64}) where {T, N}
     # Initial S2
     s2cost_init  = euclid_directional(Directional.s2(data1, 0),
                                       Directional.s2(data2, 0))
@@ -188,3 +150,15 @@ function generalized_čapek_cost(data1 :: CorrelationTracker,
 
     return f
 end
+
+"""
+    čapek_cost(data1 :: CorrelationTracker, data2 :: CorrelationTracker, η = 0.6)
+
+Returns a function which calculates the cost as based on S₂ and L₂ for
+solid and void phases where contribution of L₂ for void phase
+increases with time.
+"""
+čapek_cost(data1 :: CorrelationTracker{T, N},
+           data2 :: CorrelationTracker{T, N},
+           η     :: Float64 = 0.6) where {T, N} =
+               generalized_čapek_cost(data1, data2, Dict(T |> zero |> L2Tracker => η))
